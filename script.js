6e9484ec2d3cc41624b043e414c596324c27ab99
Germán Almaraz <germanalmaraz@hotmail.com>
2018-04-17 14:13:36 -0500

    Ya tenemos ganador

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var origBoard;
-var turn = 1;
-const huPlayer = '0';
+var turn = "X", nexT = "O";
+var tablero = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombos = [
 	[0, 1, 2],
@@ -13,13 +14,17 @@ const winCombos = [
 	[2, 4, 6],
 ]
 
+/*var casillas = [
+{numero:0, activo:0}
+]*/
+
 const cells = document.querySelectorAll('.cell');
 
 startGame();
 
 function startGame() {
-	document.querySelector(".endgame").style.display = "none"
-	origBoard = Array.from(Array(9).keys());
+	document.querySelector(".endgame").style.display = "none";
+	//origBoard = Array.from(Array(9).keys());
 	for (var i = 0; i < cells.length; i++){
 		cells [i].innerText = '';
 		cells [i].style.removeProperty('background-color');
@@ -28,19 +33,42 @@ function startGame() {
 }
 
 function turnClick(square) {
-	console.log(square.target.id);
-	if(turn==1){
+	if(cells[square.target.id].innerText != 'X' && cells[square.target.id].innerText != 'O'){
+	if(turn=="X"){
 	cells[square.target.id].innerText = 'X';
-	turn=2;
+	var targete= tablero.indexOf(square.target.id);
+tablero[targete]="X";
+turn="O";
+nexT="X";
 }
-else if(turn==2){
+else if(turn=="O"){
 	cells[square.target.id].innerText = 'O';
-	turn=1;
-}
-checale();
+	var targete= tablero.indexOf(square.target.id);
+tablero[targete]="O";
+turn="X";
+nexT="O";
 }
 
-function checale(){
-	console.log('checando');
+console.log("aqui esta el tablero" + tablero);
+checale(square, turn);
+}
+}
+
+function checale(square, turn){
+	for (var i=0; i < winCombos.length; i++){
+		if(winCombos[i][0]==square.target.id)
+			winCombos[i][0]=nexT;
+		else if(winCombos[i][1]==square.target.id)
+			winCombos[i][1]=nexT;
+		else if(winCombos[i][2]==square.target.id)
+			winCombos[i][2]=nexT;
+		if(winCombos[i][0]==nexT && winCombos[i][1]==nexT && winCombos[i][2]==nexT )
+		Ganador();
+	}
+	console.log(winCombos);
+}
+
+function Ganador(){
+	alert("El ganador es: " + nexT + "!!");
 
 }
