1f46372e9137a400094dc2811d30169d7138e456
Germán Almaraz <germanalmaraz@hotmail.com>
2018-04-20 16:59:50 -0500

    requerimientos listos

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var turn = "X", nexT = "O", CPU = 1, MasterCount=0;
-var tablero = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-var JugadaPrincipal = ["0", "2", "6", "8"];
-const winCombos = [
+var turn = "X", nexT = "O", CPU = 1, MasterCount=0, GanadorT=0, VX=0, VO=0, EMP=0;
+var tablero, winCombos;
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame() {
+	MasterCount=0
+	GanadorT=0;
+	winCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -9,20 +16,20 @@ const winCombos = [
 	[1, 4, 7],
 	[2, 5, 8],
 	[0, 4, 8],
-	[2, 4, 6],
+	[2, 4, 6]
 ]
-
-const cells = document.querySelectorAll('.cell');
-
-startGame();
-
-function startGame() {
+tablero = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+	document.getElementById('La_Tabla').style.filter="blur(0px)";
+	document.getElementById("overlay").style.display = "none";
 	document.querySelector(".endgame").style.display = "none";
+
 	for (var i = 0; i < cells.length; i++){
 		cells [i].innerText = '';
 		cells [i].style.removeProperty('background-color');
 		cells [i].addEventListener('click', turnClick, false);
 	}
+	if(turn=="O" && CPU==1)
+	Automata();
 }
 
 function turnClick(square) {
@@ -34,9 +41,10 @@ function turnClick(square) {
 tablero[targete]="X";
 turn="O";
 nexT="X";
-if(CPU==1)
 	checale(aidi, turn);
+if(CPU==1 && MasterCount<9){
 	Automata();
+}
 }
 else if(turn=="O"){
 	cells[aidi].innerText = 'O';
@@ -51,6 +59,7 @@ checale(aidi, turn);
 
 function checale(aidi, turn){
 	MasterCount++;
+
 	for (var i=0; i < winCombos.length; i++){
 		if(winCombos[i][0]==aidi)
 			winCombos[i][0]=nexT;
@@ -58,31 +67,49 @@ function checale(aidi, turn){
 			winCombos[i][1]=nexT;
 		else if(winCombos[i][2]==aidi)
 			winCombos[i][2]=nexT;
-		if(winCombos[i][0]==nexT && winCombos[i][1]==nexT && winCombos[i][2]==nexT )
+	    if(winCombos[i][0]==nexT && winCombos[i][1]==nexT && winCombos[i][2]==nexT){
+			GanadorT=nexT;
 		Ganador();
 	}
-	console.log("MasterCount: " + MasterCount);
+	}
+		if(MasterCount==9)
+		Empate();
 }
 
 function Ganador(){
-	alert("El ganador es: " + nexT + "!!");
+	if(GanadorT=="X"){
+		VX++;
+	document.getElementById("scoreX").innerText=VX;
+}
+else{
+	VO++;
+	document.getElementById("scoreO").innerText=VO;
+}
+	document.getElementById("overlay").style.display = "block";
+	document.getElementById("text").innerText="El ganador es: " + GanadorT + "!!";
+	document.getElementById('La_Tabla').style.filter="blur(5px)";
+}
 
+function Empate(){
+	EMP++;
+	document.getElementById("empates").innerText=EMP;
+	document.getElementById("overlay").style.display = "block";
+	document.getElementById("text").innerText="Empate...";
+	document.getElementById('La_Tabla').style.filter="blur(5px)";
 }
 
 function Automata(){
-	var Xcount=0;
-	var Ocount=0;
+	var Xcount=0, Ocount=0;
 	var fuera=0;
 	var IDD=9, IDD2=9, IDD3=9, IDD4=9;
-	if(tablero[4]==4){
-		IDD=4;
+
+	if(GanadorT==0){
+	 if(MasterCount<=1){
+	 	while(IDD==9||IDD=="X")
+		IDD=tablero[Math.floor(Math.random()*tablero.length)];
 		fuera=1;
 	}
-	else if(MasterCount==1){
-		IDD=JugadaPrincipal[Math.floor(Math.random()*JugadaPrincipal.length)];
-		fuera=1;
-	}
-	else
+
 	for (var i=0; i < winCombos.length; i++){
 		if(fuera==1)
 			break;
@@ -111,7 +138,7 @@ function Automata(){
 			IDD4=IDD;
 	}//el break
 	}//for externo
-	console.log("el console" + IDD + " countX " + Xcount);
+	console.log("el console: " + IDD + " countX " + Xcount);
 	if(fuera==0){
 		if(IDD2<9)
 			IDD=IDD2;
@@ -120,10 +147,31 @@ function Automata(){
 		else
 			IDD=IDD4;
 	}
-console.log(IDD);
+console.log("Tablero: " + tablero + " IDD: " + IDD);
 	tablero[IDD]="O";
 	cells[IDD].innerText = 'O';
 	turn="X";
 nexT="O";
 	checale(IDD, turn);
+}
+}
+
+function SwitchPlayers(){
+	if(CPU==1){
+	document.getElementById('VS').innerText="Player vs player";
+	document.getElementById('El_Switch').innerText="Player vs CPU";
+	CPU=0;
+}
+else if(CPU==0){
+	document.getElementById('VS').innerText="Player vs CPU";
+	document.getElementById('El_Switch').innerText="Player vs Player";
+	CPU=1;
+}
+VX=0;
+VO=0;
+EMP=0;
+document.getElementById("scoreX").innerText=VX;
+document.getElementById("scoreO").innerText=VO;
+document.getElementById("empates").innerText=EMP;
+	startGame();
 }
