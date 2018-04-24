8e4ee2fd73add82d4e283d3eaf48241ec607ac16
Germán Almaraz <germanalmaraz@hotmail.com>
2018-04-24 02:31:41 -0500

    Cambio de nombres en algunas variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var turn = "X", nexT = "O", CPU = 1, MasterCount=0, GanadorT=0, VX=0, VO=0, EMP=0;
+var TurnoActual = "X", SiguienteTurno = "O", VS_COM = 1, MasterCount=0, GanadorT=0, Victorias_X=0, Victorias_O=0, Empates=0;
 var tablero, winCombos;
 
 const cells = document.querySelectorAll('.cell');
@@ -28,62 +28,63 @@ tablero = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 		cells [i].style.removeProperty('background-color');
 		cells [i].addEventListener('click', turnClick, false);
 	}
-	if(turn=="O" && CPU==1)
-	Automata();
+	if(TurnoActual=="O" && VS_COM==1)
+	Logica_CPU();
 }
 
 function turnClick(square) {
 	var aidi=square.target.id;
 	if(cells[aidi].innerText != 'X' && cells[aidi].innerText != 'O'){
-	if(turn=="X"){
+	if(TurnoActual=="X"){
 	cells[aidi].innerText = 'X';
 	var targete= tablero.indexOf(aidi);
 tablero[targete]="X";
-turn="O";
-nexT="X";
-	checale(aidi, turn);
-if(CPU==1 && MasterCount<9){
-	Automata();
+TurnoActual="O";
+SiguienteTurno="X";
+	checale(aidi);
+if(VS_COM==1 && MasterCount<9){
+	Logica_CPU();
 }
 }
-else if(turn=="O"){
+else if(TurnoActual=="O"){
 	cells[aidi].innerText = 'O';
 	var targete= tablero.indexOf(aidi);
 tablero[targete]="O";
-turn="X";
-nexT="O";
-checale(aidi, turn);
+TurnoActual="X";
+SiguienteTurno="O";
+checale(aidi);
 }
 }
 }
 
-function checale(aidi, turn){
+function checale(aidi){
 	MasterCount++;
 
 	for (var i=0; i < winCombos.length; i++){
 		if(winCombos[i][0]==aidi)
-			winCombos[i][0]=nexT;
+			winCombos[i][0]=SiguienteTurno;
 		else if(winCombos[i][1]==aidi)
-			winCombos[i][1]=nexT;
+			winCombos[i][1]=SiguienteTurno;
 		else if(winCombos[i][2]==aidi)
-			winCombos[i][2]=nexT;
-	    if(winCombos[i][0]==nexT && winCombos[i][1]==nexT && winCombos[i][2]==nexT){
-			GanadorT=nexT;
+			winCombos[i][2]=SiguienteTurno;
+	    if(winCombos[i][0]==SiguienteTurno && winCombos[i][1]==SiguienteTurno && winCombos[i][2]==SiguienteTurno){
+			GanadorT=SiguienteTurno;
 		Ganador();
 	}
 	}
 		if(MasterCount==9)
 		Empate();
+	console.log(cells[0].innerText);
 }
 
 function Ganador(){
 	if(GanadorT=="X"){
-		VX++;
-	document.getElementById("scoreX").innerText=VX;
+		Victorias_X++;
+	document.getElementById("scoreX").innerText=Victorias_X;
 }
 else{
-	VO++;
-	document.getElementById("scoreO").innerText=VO;
+	Victorias_O++;
+	document.getElementById("scoreO").innerText=Victorias_O;
 }
 	document.getElementById("overlay").style.display = "block";
 	document.getElementById("text").innerText="El ganador es: " + GanadorT + "!!";
@@ -91,87 +92,85 @@ else{
 }
 
 function Empate(){
-	EMP++;
-	document.getElementById("empates").innerText=EMP;
+	Empates++;
+	document.getElementById("empates").innerText=Empates;
 	document.getElementById("overlay").style.display = "block";
 	document.getElementById("text").innerText="Empate...";
 	document.getElementById('La_Tabla').style.filter="blur(5px)";
 }
 
-function Automata(){
-	var Xcount=0, Ocount=0;
-	var fuera=0;
-	var IDD=9, IDD2=9, IDD3=9, IDD4=9;
+function Logica_CPU(){
+	var X_EnMismaLinea=0, O_EnMismaLinea=0;
+	var SelecciondeJugada_Lista=false;
+	var CasillaSeleccionada=9, CasillaParaGanar=9, CasillaParaNoPerder=9, CasillaParaSegundoCirculo=9, CasillaParaSeguirJugando=9;
 
 	if(GanadorT==0){
 	 if(MasterCount<=1){
-	 	while(IDD==9||IDD=="X")
-		IDD=tablero[Math.floor(Math.random()*tablero.length)];
-		fuera=1;
+	 	while(CasillaSeleccionada==9||CasillaSeleccionada=="X")
+		CasillaSeleccionada=tablero[Math.floor(Math.random()*tablero.length)];
+
+		SelecciondeJugada_Lista=true;
 	}
 
 	for (var i=0; i < winCombos.length; i++){
-		if(fuera==1)
+		if(SelecciondeJugada_Lista==true)
 			break;
 		else{
-		Xcount=0;
-		Ocount=0;
-		for(var t=0; t< winCombos[i].length; t++ ){
-			if(winCombos[i][t]=="X")
-			Xcount++;
-		else if(winCombos[i][t]=="O")
-			Ocount++;
+		X_EnMismaLinea=0;
+		O_EnMismaLinea=0;
+		for(var j=0; j< winCombos[i].length; j++ ){
+			if(winCombos[i][j]=="X")
+			X_EnMismaLinea++;
+		else if(winCombos[i][j]=="O")
+			O_EnMismaLinea++;
 		else
-		IDD=winCombos[i][t];
+		CasillaSeleccionada=winCombos[i][j];
 
-		console.log(winCombos[i] + " | " + winCombos[i][t] + " | " +Xcount + " | " + Ocount);
-
-	}//for interno
-
-	       if(Xcount==0&&Ocount==2)
-	      fuera=1;
-         if(Xcount==2&&Ocount==0)
-            IDD2=IDD;
-		if(Xcount==0&&Ocount==1)
-			IDD3=IDD;
-		if(Xcount<=1&&Ocount==1)
-			IDD4=IDD;
-	}//el break
-	}//for externo
-	console.log("el console: " + IDD + " countX " + Xcount);
-	if(fuera==0){
-		if(IDD2<9)
-			IDD=IDD2;
-	     else if(IDD3<9)
-			IDD=IDD3;
-		else
-			IDD=IDD4;
 	}
-console.log("Tablero: " + tablero + " IDD: " + IDD);
-	tablero[IDD]="O";
-	cells[IDD].innerText = 'O';
-	turn="X";
-nexT="O";
-	checale(IDD, turn);
+
+	       if(X_EnMismaLinea==0&&O_EnMismaLinea==2)
+	      SelecciondeJugada_Lista=true;
+         if(X_EnMismaLinea==2&&O_EnMismaLinea==0)
+            CasillaParaNoPerder=CasillaSeleccionada;
+		if(X_EnMismaLinea==0&&O_EnMismaLinea==1)
+			CasillaParaSegundoCirculo=CasillaSeleccionada;
+		if(X_EnMismaLinea<=1&&O_EnMismaLinea==1)
+			CasillaParaSeguirJugando=CasillaSeleccionada;
+	}
+	}
+	if(SelecciondeJugada_Lista==false){
+		if(CasillaParaNoPerder<9)
+			CasillaSeleccionada=CasillaParaNoPerder;
+	     else if(CasillaParaSegundoCirculo<9)
+			CasillaSeleccionada=CasillaParaSegundoCirculo;
+		else
+			CasillaSeleccionada=CasillaParaSeguirJugando;
+	}
+
+	tablero[CasillaSeleccionada]="O";
+	cells[CasillaSeleccionada].innerText = 'O';
+	TurnoActual="X";
+SiguienteTurno="O";
+	checale(CasillaSeleccionada);
 }
 }
 
 function SwitchPlayers(){
-	if(CPU==1){
+	if(VS_COM==1){
 	document.getElementById('VS').innerText="Player vs player";
 	document.getElementById('El_Switch').innerText="Player vs CPU";
-	CPU=0;
+	VS_COM=0;
 }
-else if(CPU==0){
+else if(VS_COM==0){
 	document.getElementById('VS').innerText="Player vs CPU";
 	document.getElementById('El_Switch').innerText="Player vs Player";
-	CPU=1;
+	VS_COM=1;
 }
-VX=0;
-VO=0;
-EMP=0;
-document.getElementById("scoreX").innerText=VX;
-document.getElementById("scoreO").innerText=VO;
-document.getElementById("empates").innerText=EMP;
+Victorias_X=0;
+Victorias_O=0;
+Empates=0;
+document.getElementById("scoreX").innerText=Victorias_X;
+document.getElementById("scoreO").innerText=Victorias_O;
+document.getElementById("empates").innerText=Empates;
 	startGame();
 }
